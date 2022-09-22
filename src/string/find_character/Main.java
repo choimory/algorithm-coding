38151bfb1ee101359ba0ed21880ef2e95a6d28ec
choimory <choimory@naver.com>
2022-09-22 21:37:27 +0900

    feat: 문자열 찾기

--- src/string/find_character/Main.java
+++ src/string/find_character/Main.java
@@ -1,6 +1,24 @@
 package string.find_character;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next().toUpperCase(Locale.ROOT);
+        String target = scanner.next().toUpperCase(Locale.ROOT);
+        System.out.print(main.solution(word, target));
+    }
+
+    public int solution(String word, String target){
+        int result = 0;
+        for (char c : word.toCharArray()) {
+            if(c == target.toCharArray()[0]){
+                result++;
+            }
+        }
+        return result;
     }
 }
